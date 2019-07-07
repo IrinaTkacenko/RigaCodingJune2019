7c7cbc44c54f83f4266e1893a0db298d1611a446
IrinaTkacenko <52213068+IrinaTkacenko@users.noreply.github.com>
2019-07-07 14:19:17 +0300

    TheGuessingGameHalfCompleted

---
+++ Day7/src/day7/Day7.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day7;
+
+/**
+ *
+ * @author User
+ */
+public class Day7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // 1. Uzt. jaunu formu
+        // 2. Uzt. vienu ievades lauku, un vienupogu, izvades lauks
+        // 3. 1. speletajs ievada minamo vardu
+        // 4. Tad kad ir noticis,  un nospiesta poga, vards pazud
+        // 5. paradas pazinojums
+        // 6. 2. speletajs censas uzminet vardu
+        // 7. lauj minet 5 reizes, ja neuzmin, tad tad izvada pazinojumu, tad pirmais atkal
+        // var sakt
+        
+        // hinti  - vardu saglabat globalaja string mainigaja, uzminetas reizes ari mainigaja
+        // (ne string), visu darbibu kontrole poga
+        
+        // 8. paradas uzminetie burti
+        
+        TheGuessingGame play = new TheGuessingGame();
+        play.setVisible(true);
+    }
+    
+}
